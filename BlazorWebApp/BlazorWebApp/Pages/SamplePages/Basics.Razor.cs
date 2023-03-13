ea25e7bb1e26d28579b0c25f8c566c8b1fc44efe
apalavecinofrez1 <apalavecinofrez1@WC322-08>
2023-03-13 14:40:14 -0600

    blazor day 3

--- BlazorWebApp/BlazorWebApp/Pages/SamplePages/Basics.Razor.cs
+++ BlazorWebApp/BlazorWebApp/Pages/SamplePages/Basics.Razor.cs
@@ -4,14 +4,21 @@
     {
         #region Fields
         private string myName;
+        private int oddEven;
         #endregion
         //  Method invokes when the component is ready to start
         //      having recieved it's initial paramaters from it's parent in the render tree
         protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
+            RandomValue();
+            
+        }
+
+        private void RandomValue()
+        {
             Random rnd = new Random();
-            int oddEven = rnd.Next(0, 25);
+            oddEven = rnd.Next(0, 25);
             if (oddEven % 2 == 0)
             {
                 myName = $"Alan is even {oddEven}";
---
+++ BlazorWebApp/BlazorWebApp/Pages/SamplePages/Invoice.razor.cs
@@ -0,0 +1,34 @@
+﻿using ChinookSystem;
+
+namespace BlazorWebApp.Pages.SamplePages
+{
+    public partial class Invoice
+    {
+        #region Fields
+        private InvoiceView invoice;
+        private string feedBack;
+        private int counter = 1;
+        #endregion
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            invoice = new InvoiceView();
+            Random rnd = new Random();
+            invoice.InvoiceNo = rnd.Next(1000, 2000);
+        }
+
+        private void HandleSubmit()
+        {
+            feedBack = $"Submitted Press = {counter++}";
+        }
+        private void HandleValidSubmit()
+        {
+            feedBack = $"Valid submission = {counter++}";
+        }
+        private void HandleInvalidSubmit()
+        {
+            feedBack = $"InValid submission = {counter++}";
+        }
+    }
+}
